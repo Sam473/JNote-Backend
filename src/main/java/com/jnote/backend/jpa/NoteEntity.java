557c88ff2b5219b46e260187e63e2b1468bbcb4a
Samuel Stacey <ss3467@bath.ac.uk>
2021-12-04 01:30:32 +0000

    Done: -fix h2 errors
    Next:
    -MVC tests
    -Update all tests

--- src/main/java/com/jnote/backend/jpa/NoteEntity.java
+++ src/main/java/com/jnote/backend/jpa/NoteEntity.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public class NoteEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private final String title;
