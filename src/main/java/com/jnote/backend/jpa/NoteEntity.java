ed16d5aa358dd6e53b8a100ff15f83e3753ffdcf
Samuel Stacey <ss3467@bath.ac.uk>
2021-12-04 00:58:51 +0000

    Done: -fix h2 errors
    Next:
    -MVC tests
    -Update all tests

--- src/main/java/com/jnote/backend/jpa/NoteEntity.java
+++ src/main/java/com/jnote/backend/jpa/NoteEntity.java
@@ -1,9 +1,6 @@
 package com.jnote.backend.jpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -11,7 +8,7 @@ import java.time.LocalDateTime;
 public class NoteEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private final String title;
